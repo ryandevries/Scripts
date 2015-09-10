16b507a4371e119265404db7838843f4b60dff69
Ryan DeVries <rj@ryandevries.com>
2015-09-10 12:21:16 -0400

    Reworked logic

--- SQL/Inventory/Get_SqlInstanceInfo.sql
+++ SQL/Inventory/Get_SqlInstanceInfo.sql
@@ -1,77 +1,78 @@
-DECLARE @SQLDECLARE varchar(8000)
-	, @SQLSET1 varchar(8000)
-	, @SQLSET2 varchar(8000)
-	, @SQLSELECT varchar(8000)
+DECLARE @SERVERINFO_BASIC_TSQL   VARCHAR(8000)
+DECLARE @OSSTATS_SQL2000_TSQL    VARCHAR(8000)
+DECLARE @SERVERINFO_SQL2000_TSQL VARCHAR(8000)
+DECLARE @SERVERINFO_SQL2005_TSQL VARCHAR(8000)
+DECLARE @SERVERINFO_TSQL         VARCHAR(8000)
 
-SET @SQLDECLARE = '
-DECLARE @VERSION varchar(50)
-	, @BUILD varchar(50)
-	, @BUILDNUMBER varchar(50)
-	, @EDITION varchar(50)
-	, @AUTHENTICATION varchar(50)
-	, @SERVERNAME varchar(50)
-	, @INSTANCENAME varchar(50)
-	, @MEMORYALLOCATEDMB bigint
-	, @MAXDOP int
-	, @CTFP int
-	, @CORES int
-	, @TOTALMEMORYMB bigint
-	, @STARTUPTIME datetime'
+SET @SERVERINFO_BASIC_TSQL = '
+SELECT 
+	CONVERT(VARCHAR(50),(SELECT SERVERPROPERTY(''MachineName'')))    AS [ServerName],
+	CONVERT(VARCHAR(50),(SELECT SERVERPROPERTY(''InstanceName'')))   AS [InstanceName],
+	CONVERT(VARCHAR(50),(SELECT 
+		CASE (SELECT LEFT(CAST(SERVERPROPERTY(''ProductVersion'') AS VARCHAR), 4))
+			WHEN ''13.0'' THEN ''SQL Server 2016''
+            WHEN ''12.0'' THEN ''SQL Server 2014''
+			WHEN ''11.0'' THEN ''SQL Server 2012''
+			WHEN ''10.5'' THEN ''SQL Server 2008 R2''
+			WHEN ''10.0'' THEN ''SQL Server 2008''
+			WHEN ''9.00'' THEN ''SQL Server 2005''
+			WHEN ''8.00'' THEN ''SQL Server 2000''
+			ELSE ''Unknown Version'' 
+		END
+		)
+	)                                                                AS [Version],
+	CONVERT(VARCHAR(50),(SELECT SERVERPROPERTY(''ProductLevel'')))   AS [Build],
+	CONVERT(VARCHAR(50),(SELECT SERVERPROPERTY(''ProductVersion''))) AS [BuildNumber],
+	CONVERT(VARCHAR(50),(SELECT SERVERPROPERTY(''Edition'')))        AS [Edition],
+	CONVERT(VARCHAR(50),(SELECT 
+		CASE (SELECT SERVERPROPERTY(''IsIntegratedSecurityOnly'')) 
+			WHEN 1 THEN ''Windows'' 
+			WHEN 0 THEN ''Mixed Mode'' 
+		END
+		)
+	)                                                                AS [Authentication],
+	GETDATE()                                                        AS [Timestamp],
+'
+SET @OSSTATS_SQL2000_TSQL = '
+IF OBJECT_ID(''tempdb..##OSstats'') IS NOT NULL
+	DROP TABLE ##OSstats
+CREATE TABLE ##OSstats ([Index] VARCHAR(2000), [Name] VARCHAR(2000), [Internal_Value] VARCHAR(2000), [Character_Value] VARCHAR(2000)) 
+INSERT INTO  ##OSstats EXEC xp_msver'
 
-SET @SQLSET1 = '
-SET @SERVERNAME = CONVERT(varchar(50),(SELECT SERVERPROPERTY(''MachineName'')))
-	SET @INSTANCENAME = CONVERT(varchar(50),(SELECT SERVERPROPERTY(''InstanceName'')))
-	SET @VERSION = CONVERT(varchar(50),(SELECT CASE (SELECT LEFT(CAST(SERVERPROPERTY(''ProductVersion'') AS VARCHAR), 4))
-		WHEN ''13.0'' THEN ''SQL Server 2016''
-		WHEN ''12.0'' THEN ''SQL Server 2014''
-		WHEN ''11.0'' THEN ''SQL Server 2012''
-		WHEN ''10.5'' THEN ''SQL Server 2008 R2''
-		WHEN ''10.0'' THEN ''SQL Server 2008''
-		WHEN ''9.00'' THEN ''SQL Server 2005''
-		WHEN ''8.00'' THEN ''SQL Server 2000''
-		ELSE ''ERROR'' END)) 
-	SET @BUILD = CONVERT(varchar(50),(SELECT SERVERPROPERTY(''ProductLevel'')))
-	SET @BUILDNUMBER = CONVERT(varchar(50),(SELECT SERVERPROPERTY(''ProductVersion'')))
-	SET @EDITION = CONVERT(varchar(50),(SELECT SERVERPROPERTY(''Edition'')))
-	SET @AUTHENTICATION = CONVERT(varchar(50),(SELECT CASE (SELECT SERVERPROPERTY(''IsIntegratedSecurityOnly'')) WHEN 1 THEN ''Windows'' WHEN 0 THEN ''Mixed Mode'' END))'
+SET @SERVERINFO_SQL2000_TSQL = '
+CONVERT(BIGINT,(SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1544'')) AS [MemoryAllocatedMB],
+CONVERT(INT,   (SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1539'')) AS [MaxDOP],
+CONVERT(INT,   (SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1538'')) AS [CTFP],
+(SELECT [Internal_Value] FROM ##OSstats WHERE [name] = ''ProcessorCount'')                     AS [Cores],
+(SELECT [Internal_Value] FROM ##OSstats WHERE [name] = ''PhysicalMemory'')                     AS [TotalMemoryMB],
+(SELECT [crdate] FROM [master].[dbo].[sysdatabases] WHERE [name] = ''tempdb'')                 AS [StartupTime]'
 
-IF LEFT(CAST(SERVERPROPERTY('ProductVersion') AS varchar),1)='8'
-BEGIN	
-    SET @SQLSET2 = '
-	IF OBJECT_ID(''tempdb..#tmpOSstats'') IS NOT NULL
-		DROP TABLE #tmpOSstats
-	CREATE TABLE #tmpOSstats ([Index] VARCHAR(2000), [Name] VARCHAR(2000), [Internal_Value] VARCHAR(2000), [Character_Value] VARCHAR(2000)) 
-	INSERT INTO #tmpOSstats EXEC xp_msver
-	
-	SET @MEMORYALLOCATEDMB = CONVERT(bigint,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1544''))
-    SET @MAXDOP = CONVERT(int,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1539''))
-    SET @CTFP = CONVERT(int,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1538''))
-	SET @CORES = (SELECT Internal_Value FROM #tmpOSstats WHERE Name = ''ProcessorCount'')
-	SET @TOTALMEMORYMB = (SELECT Internal_Value FROM #tmpOSstats WHERE Name = ''PhysicalMemory'')
-	SET @STARTUPTIME = (SELECT crdate FROM [master].[dbo].[sysdatabases] WHERE name = ''tempdb'')'
+SET @SERVERINFO_SQL2005_TSQL = '
+CONVERT(BIGINT,(SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1544'')) AS [MemoryAllocatedMB],
+CONVERT(INT,   (SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1539'')) AS [MaxDOP],
+CONVERT(INT,   (SELECT [value] FROM [master].[dbo].[sysconfigures] WHERE [config] = ''1538'')) AS [CTFP],
+(SELECT [cpu_count] FROM [master].[sys].[dm_os_sys_info])                                      AS [Cores],
+(SELECT [physical_memory_in_bytes]/1024/1024 FROM [master].[sys].[dm_os_sys_info])             AS [TotalMemoryMB],
+(SELECT [create_date] FROM [master].[sys].[databases] WHERE [name] = ''tempdb'')               AS [StartupTime]'
+
+SET @SERVERINFO_TSQL = '
+CONVERT(BIGINT,(SELECT [value] FROM [master].[sys].[configurations] WHERE [configuration_id] = ''1544'')) AS [MemoryAllocatedMB],
+CONVERT(INT,(   SELECT [value] FROM [master].[sys].[configurations] WHERE [configuration_id] = ''1539'')) AS [MaxDOP],
+CONVERT(INT,(   SELECT [value] FROM [master].[sys].[configurations] WHERE [configuration_id] = ''1538'')) AS [CTFP],
+(SELECT [cpu_count] FROM [master].[sys].[dm_os_sys_info])												  AS [Cores],
+(SELECT [total_physical_memory_kb]/1024 FROM [master].[sys].[dm_os_sys_memory])						      AS [TotalMemoryMB],
+(SELECT [sqlserver_start_time] FROM [master].[sys].[dm_os_sys_info])									  AS [StartupTime]'
+
+IF LEFT(CAST(SERVERPROPERTY('ProductVersion') AS VARCHAR),1)='8'
+BEGIN
+	EXEC (@OSSTATS_SQL2000_TSQL)
+	EXEC (@SERVERINFO_BASIC_TSQL + @SERVERINFO_SQL2000_TSQL)
 END
-ELSE IF LEFT(CAST(SERVERPROPERTY('ProductVersion') AS varchar),1)='9'
+ELSE IF LEFT(CAST(SERVERPROPERTY('ProductVersion') AS VARCHAR),1)='9'
 BEGIN	
-    SET @SQLSET2 = '
-	SET @MEMORYALLOCATEDMB = CONVERT(bigint,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1544''))
-    SET @MAXDOP = CONVERT(int,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1539''))
-    SET @CTFP = CONVERT(int,(SELECT value FROM [master].[dbo].[sysconfigures] WHERE config = ''1538''))
-	SET @CORES = (SELECT cpu_count FROM [master].[sys].[dm_os_sys_info])
-	SET @TOTALMEMORYMB = (SELECT physical_memory_in_bytes/1024/1024 FROM [master].[sys].[dm_os_sys_info])
-	SET @STARTUPTIME = (SELECT create_date FROM [master].[sys].[databases] WHERE name = ''tempdb'')'
+	EXEC (@SERVERINFO_BASIC_TSQL + @SERVERINFO_SQL2005_TSQL) 
 END
 ELSE
 BEGIN
-	SET @SQLSET2 = '
-	SET @MEMORYALLOCATEDMB = CONVERT(bigint,(SELECT value FROM [master].[sys].[configurations] WHERE configuration_id = ''1544''))
-    SET @MAXDOP = CONVERT(int,(SELECT value FROM [master].[sys].[configurations] WHERE configuration_id = ''1539''))
-    SET @CTFP = CONVERT(int,(SELECT value FROM [master].[sys].[configurations] WHERE configuration_id = ''1538''))
-	SET @CORES = (SELECT cpu_count FROM [master].[sys].[dm_os_sys_info])
-	SET @TOTALMEMORYMB = (SELECT [total_physical_memory_kb]/1024 FROM [master].[sys].[dm_os_sys_memory])
-	SET @STARTUPTIME = (SELECT sqlserver_start_time FROM [master].[sys].[dm_os_sys_info])'
+	EXEC (@SERVERINFO_BASIC_TSQL + @SERVERINFO_TSQL)
 END
-
-SET @SQLSELECT = '
-SELECT @SERVERNAME AS ServerName, @INSTANCENAME AS InstanceName, @VERSION AS Version, @BUILD AS Build, @BUILDNUMBER AS BuildNumber, @EDITION AS Edition, @AUTHENTICATION AS Authentication, @CORES AS Cores, @TOTALMEMORYMB AS TotalMemoryMB, @MEMORYALLOCATEDMB AS MemoryAllocatedMB, @MAXDOP AS MaxDOP, @CTFP AS CTFP, @STARTUPTIME AS StartupTime, getdate() AS Timestamp'
-
-EXEC (@SQLDECLARE + @SQLSET1 + @SQLSET2 + @SQLSELECT)
